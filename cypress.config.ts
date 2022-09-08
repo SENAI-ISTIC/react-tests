1f19efc4e0d6e633fb146096dca0a6a7f6646a4c
ElderGr <gbielrs@gmail.com>
2022-09-08 14:06:28 -0300

    fix: moved folder of e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    specPattern: 'src/__test__/e2e/*.cy{.ts,.tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
